352378c1075cfcbbaa591d3ba22d1b1afaf306b4
Kaxaru <khidechi@gmail.com>
2020-02-20 13:12:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+    ZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six','seven', 'eight', 'nine','ten', 'eleven', 'twelve', 'thirteen','fourteen','fifteen', 'sixteen','seventeen', 'eighteen', 'nineteen'];
+    Tenners = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ]
+    numberToString = (number) => {
+        return number < 20 ? ZeroToNineteen[number] : number < 100 ? `${Tenners[Math.floor(number / 10) - 2]}${number % 10 == 0 ? '': ' ' + ZeroToNineteen[number % 10]}` : null 
+    }
+    return (number >= 100) ? `${ZeroToNineteen[Math.floor(number / 100)]} hundred${(number - Math.floor(number / 100) * 100) == 0 ? '' :' ' + numberToString(number - Math.floor(number / 100) * 100)}` : numberToString(number)   
 }
